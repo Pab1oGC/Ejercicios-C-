3b199d8d107ba99124dc60a546882b680acab728
Pablo Gutierrez L <pablogutierrrez0@gmail.com>
2024-01-02 18:25:20 -0400

    simulacion de juego de tenis

---
+++ juegoDeTenis/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Retos;
+public class Jugador
+{
+    public string Puntos { get; set; }
+}
+/*
+ * Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+ * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+ * gane cada punto del juego.
+ * 
+ * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+ * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+ *   15 - Love
+ *   30 - Love
+ *   30 - 15
+ *   30 - 30
+ *   40 - 30
+ *   Deuce
+ *   Ventaja P1
+ *   Ha ganado el P1
+ * - Si quieres, puedes controlar errores en la entrada de datos.   
+ * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+ */
+public class RetoDos
+{
+    public static void Main(string[] args)
+    {
+        Jugador jugador1 = new Jugador();
+        Jugador jugador2 = new Jugador();
+        string[] puntuaciones = { "Love", "15", "30", "40", "Ganó"};
+        Console.WriteLine("Set 1");
+        Juego(jugador1, jugador2, puntuaciones);
+    }
+    public static void Juego(Jugador j1,Jugador j2, string[] puntuaciones)
+    {
+        Random random = new Random();
+        int turno, indice1, indice2;
+        turno = indice1 = indice2 = 0;
+        j1.Puntos = puntuaciones[indice1];
+        j2.Puntos = puntuaciones[indice2];
+        while (indice1 != 4 && indice2 != 4)
+        {
+            turno++;
+            int randomico = random.Next(0, 2);
+            if(turno%2 == 0)
+            {
+                indice2 += randomico;
+                j2.Puntos = puntuaciones[indice2];
+                Console.WriteLine("Turno Jugador 2");
+            }
+            else
+            {
+                indice1 += randomico;
+                j1.Puntos = puntuaciones[indice1];
+                Console.WriteLine("Turno Jugador 1");
+            }
+            if(indice1 != 4 && indice2 !=4)
+            {
+                Console.ReadKey();
+                Console.WriteLine($"{j1.Puntos} - {j2.Puntos}");
+                Console.ReadKey();
+            }  
+        }
+        string mensajeGanador = j1.Puntos == "Ganó" ? $"Ganador del Set: Jugador 1" : $"Ganador del Set: Jugador 2";
+        Console.ReadKey();
+        Console.WriteLine(mensajeGanador);
+    }
+}
---
+++ practicasVacaciones/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace practicasVacaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for(int i=1; i<=100;i++)
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else  
+                {
+                    if(i % 3 == 0)
+                    { 
+                    Console.WriteLine("fizz");
+                    }
+                    else
+                    {
+                        if(i%5==0)
+                        {
+                            Console.WriteLine("buzz");
+                        }
+                        else
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+                
+            }*/
+            /*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+            string[] abcLeet = {"4","I3","[",")",
+            "3","|=","&","#","1",",_|",">|","£","[V]",
+            "^/","0","|*","(_,)","I2","5","7","(_)",
+            "|/","VV","><","j","2","L","R","E","A","S",
+            "b","T","B","g","o"};
+            char[] abcNormal = { 'A', 'B', 'C','D','E',
+            'F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z','1',
+            '2','3','4','5','6','7','8','9','0'};
+            Console.WriteLine("Ingrese la cadena que desea cambiar a lenguaje hacker:");
+            string cadena=(Console.ReadLine()!).ToUpper();
+            string nuevaCadena = "";
+            for(int i = 0; i < cadena.Length; i++)
+            {
+                char hackeado = char.Parse(cadena.Substring(i,1));
+                for(int j = 0;j < abcNormal.Length;j++)
+                {
+                    if (hackeado == abcNormal[j])
+                    {
+                        nuevaCadena += abcLeet[j];
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("La nueva cadena es:");
+            Console.WriteLine(nuevaCadena);
+        }
+    }
+}
